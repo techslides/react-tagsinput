3630577c32d2bd4d8d4ce0ec65a7879a12d8040f
Ola Holmström <ola.holmstrom@gmail.com>
2015-01-31 14:50:02 +0100

    begin work on support for completion and add props and methods proposed by ztmr

--- react-tagsinput.js
+++ react-tagsinput.js
@@ -49,6 +49,8 @@
         , validate: function (tag) { return tag !== ""; }
         , addKeys: [13, 9]
         , removeKeys: [8]
+        , onBeforeTagAdd: function () { return true; }
+        , onBeforeTagRemove: function () { return true; }
         , onTagAdd: function () { }
         , onTagRemove: function () { }
         , onChange: function () { }
@@ -74,10 +76,10 @@
       return this.state.tags;
     }
 
-    , addTag: function () {
-      var tag = this.state.tag.trim();
+    , addTag: function (tag) {
+      var valid = this.props.onBeforeTagAdd(tag) && this.props.validate(tag);
 
-      if (this.state.tags.indexOf(tag) !== -1 || !this.props.validate(tag)) {
+      if (this.state.tags.indexOf(tag) !== -1 || !valid) {
         return this.setState({
           invalid: true
         });
@@ -94,14 +96,21 @@
       });
     }
 
-    , removeTag: function (i) {
-      var tags = this.state.tags.slice(0);
-      var tag = tags.splice(i, 1);
+    , removeTag: function (tag) {
+      var valid = this.props.onBeforeTagRemove(tag);
+
+      if (!valid) { return ; }
+
+      var tags = this.state.tags.slice(0)
+        , i = tags.indexOf(tag);
+
+      tags.splice(i, 1);
+
       this.setState({
         tags: tags
         , invalid: false
       }, function () {
-        this.props.onTagRemove(tag[0]);
+        this.props.onTagRemove(tag);
         this.props.onChange(this.state.tags);
       });
     }
@@ -112,11 +121,11 @@
 
       if (add) {
         e.preventDefault();
-        this.addTag();
+        this.addTag(this.state.tag.trim());
       }
 
       if (remove && this.state.tags.length > 0 && this.state.tag === "") {
-        this.removeTag(this.state.tags.length - 1);
+        this.removeTag(this.state.tags[this.state.tags.length - 1]);
       }
     }
 
@@ -129,7 +138,7 @@
 
     , onBlur: function (e) {
       if (this.state.tag !== "" && !this.state.invalid) {
-        this.addTag();
+        this.addTag(this.state.tag.trim());
       }
     }
 
@@ -145,7 +154,7 @@
           key: i
           , ns: ns
           , tag: tag
-          , remove: this.removeTag.bind(null, i)
+          , remove: this.removeTag.bind(null, tag)
         });
       }.bind(this));
 
